15ba65ee875efff1b88ae801d121e19c085d0cbb
Roman Dvorak <roman@telescope.(none)>
2015-09-18 21:34:35 +0000

    RA motor device driver

--- RA_HBSTEP_driver/server.py
+++ RA_HBSTEP_driver/server.py
@@ -1,51 +1,166 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import socket
 import sys
 import time
 from pymlab import config
 from datetime import datetime
 import ConfigParser
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+from tornado import httpserver
+from tornado import websocket
+from tornado import ioloop
+from tornado import web
+import socket
+import netifaces as ni
+
+class WSHandler(websocket.WebSocketHandler):
+    def get(self):
+        print "Aaaa"
+
+    def open(self):
+        print 'connection opened...'
+        self.write_message("The server says: 'Hello'. Connection was accepted.")
+
+    def on_message(self, message):
+        self.write_message("The server says: " + message + " back at you")
+        print 'received:', message
+
+    def on_close(self):
+        print 'connection closed...'
+
 
 
 class RAserver():
-	def __init__(self, arg):
-		super(RAserver, self).__init__()
-		self.arg = arg
+    def __init__(self, port=10123 , lIP="127.0.0.1"):
+        self.port = port
+        self.lIP = lIP
+        cfg = config.Config(
+            i2c = {
+                "port": 1,
+            },
+            bus = [
+                { "name":"spi", "type":"i2cspi"},
+                { "name":"lcd", "type":"i2clcd"},
+                { "name":"sht", "type":"sht25"},
+                { "name":"lts", "type":"lts01"},
+            ],
+        )
 
-		cfg = config.Config(
-			i2c = {
-				"port": 8,
-			},
-			bus = [
-				{ "name":"spi", "type":"i2cspi"},
-				{ "name":"lcd", "type":"i2clcd"},
-				{ "name":"sht", "type":"sht25"},
-				{ "name":"lts", "type":"lts01"},
-			],
-		)
+        n = 0
+        cfg.initialize()
+        self.lcd = cfg.get_device("lcd")
+        self.spi = cfg.get_device("spi")
+        self.sht = cfg.get_device("sht")
+        self.lts = cfg.get_device("lts")
+        self.lcd.light(1)
 
-		n = 0
-		cfg.initialize()
-		self.spi = cfg.get_device("spi")
-		self.lcd = cfg.get_device("lcd")
-		self.sht = cfg.get_device("sht")
-		self.lts = cfg.get_device("lts")
+        time.sleep(0.5)
+        self.spi.SPI_config(self.spi.I2CSPI_MSB_FIRST| self.spi.I2CSPI_MODE_CLK_IDLE_HIGH_DATA_EDGE_TRAILING| self.spi.I2CSPI_CLK_461kHz)
+        time.sleep(0.5)
 
-		spi.SPI_config(spi.I2CSPI_MSB_FIRST| spi.I2CSPI_MODE_CLK_IDLE_HIGH_DATA_EDGE_TRAILING| spi.I2CSPI_CLK_461kHz)
+        self.lcd.reset()
+        self.lcd.init()
+        time.sleep(0.1)
+        self.lcd.clear()
+        time.sleep(0.1)
+        self.lcd.home()
+        self.lcd.puts("Loading")
+        self.lcd.set_row2()
+        self.lcd.puts("RAmotor-TT exten")
 
-
-
-		lcd.clear()
-		lcd.home()
-		lcd.puts("RAserver")
-		lcd.set_row2()
-		lcd.puts("loaded")
+        time.sleep(1)
 
 
+    def CreateServer(self):
 
-def  main():
-	ra = RAserver()
+        self.lcd.reset()
+        self.lcd.init()
+        time.sleep(0.1)
+        self.lcd.clear()
 
-if __name__ == '__main__':
-	main()
+        self.app = web.Application([
+            (r'/', IndexHandler),
+            (r'/ws', SocketHandler, {'parent': self}),
+        ], debug=True)
+        self.app.listen(self.port)
+
+        self.lcd.home()
+        self.lcd.puts("Loading 01")
+
+        tornado.ioloop.IOLoop.current().start()
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        print "get WWW"
+        self.render("index.html")
+cl = []
+
+class SocketHandler(websocket.WebSocketHandler): 
+    def initialize(self, parent):
+        self.ra = parent
+        self.StationList = []
+        self.ra.lcd.puts("X")
+
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print "Opened new port: ", self
+        if self not in cl:
+            cl.append(self)
+        #self.write_message("HI")
+
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+    def on_message(self, message):
+        print "Prijata zprava: ", message
+        #self.write_message(u"ACK")
+        if message[0] == "$":
+            self.ra.lcd.light(1)
+            m_type = message[1:message.find(";")].lower()
+            print "data msg", m_type
+            if m_type == "hi":
+                self.StationList.append([self] + message[message.find(";")+1:].split(";") )
+                print self.StationList
+            elif m_type == "sht25":
+                print "sht"
+                temp = self.ra.sht.get_temp()
+                humidity = self.ra.sht.get_hum()
+                print "pozadavek o SHT t:", temp, " h:", humidity
+                self.write_message(u"&sht25;%f;%f;" %(temp, humidity))
+            elif m_type == "lts":
+                print "lts"
+                temp = self.ra.lts.get_temp()
+                print "pozadavek o LTS t:", temp
+                self.write_message(u"&lts;%f;" %(temp))
+            else:
+                print "Neznama dolarova operace"
+                self.write_message(u"&None")
+
+        else:
+            print "unsupported"
+            self.write_message(u"&unsupported")
+        #    for client in cl:
+        #        client.write_message(u"multicast: " + message)
+        self.ra.lcd.light(0)
+
+
+
+def main():
+    ip = ni.ifaddresses('eth0')[2][0]['addr']
+    port = 10123
+    print ip, port
+    ra = RAserver(port, ip)
+    ra.CreateServer()
+
+if __name__ == "__main__":
+    main()
